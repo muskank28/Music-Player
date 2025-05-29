fb7e688074ba185603cc996e31c970a1d95006a9
Muskan <muskikhan282004@gmail.com>
2025-05-29 23:19:53 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Music-Player
+A sleek, modern music player with smooth animations, intuitive controls, and responsive design—built with pure HTML, CSS, and JavaScript.
---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'cover 1.jpg'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'cover 3.jpg'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'cover 4.jpg'
+    },
+    {
+        name: 'song 5',
+        path: 'musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'cover 5.png'
+    },
+]
